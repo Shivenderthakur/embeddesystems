630f81210abb8541b1ecd7403e3c2c8e102a7719
ShivenderThakur <shivthakur4512@gmail.com>
2025-03-27 02:38:44 -0700

    adding the comment needed

--- RoboticArmESP32/RoboticArmESP32.ino
+++ RoboticArmESP32/RoboticArmESP32.ino
@@ -2,17 +2,17 @@
 #include <ESP32Servo.h>
 #include <BluetoothSerial.h>
 
-#define SERVO_BASE            2
-#define SERVO_SHOULDER        3
-#define SERVO_ELBOW           4
-#define SERVO_GRIPPER         5
+#define SERVO_BASE            13
+#define SERVO_SHOULDER        12
+#define SERVO_ELBOW           14
+#define SERVO_GRIPPER         27
 
 BluetoothSerial SerialBT; // Create BluetoothSerial object
 
-Servo myservo_1;  // create servo object to control a servo
-Servo myservo_2;
-Servo myservo_3;
-Servo myservo_4;
+Servo myservo_1;  // Servo for base movement
+Servo myservo_2;  // Servo for shoulder movement
+Servo myservo_3;  // Servo for elbow movement
+Servo myservo_4;  // Servo for gripper control
 
 unsigned char Data_String[25], Data_Index = 0, New_Data_Rec_Flag = 0;
 unsigned int Received_Servo_Value[4], Final_Servo_Val[4];
@@ -22,11 +22,10 @@ void setup() {
   Serial.begin(9600);  // Initialize Serial Monitor communication
   SerialBT.begin("ROBOARM"); // Set the Bluetooth device name
 
-  myservo_1.attach(SERVO_BASE);       // attaches the servo on pin 2 to the servo object
-  myservo_2.attach(SERVO_SHOULDER);   // attaches the servo on pin 3 to the servo object
-  myservo_3.attach(SERVO_ELBOW);      // attaches the servo on pin 4 to the servo object
-  myservo_4.attach(SERVO_GRIPPER);    // attaches the servo on pin 5 to the servo object
-
+  myservo_1.attach(SERVO_BASE);       
+  myservo_2.attach(SERVO_SHOULDER);   
+  myservo_3.attach(SERVO_ELBOW);      
+  myservo_4.attach(SERVO_GRIPPER);    
   myservo_1.write(90);
   delay(200);
   myservo_2.write(90);
@@ -66,7 +65,7 @@ void loop() {
     sscanf((char *)Data_String, "%d,%d,%d,%d", &Received_Servo_Value[0], 
            &Received_Servo_Value[1], &Received_Servo_Value[2], &Received_Servo_Value[3]);
 
-    // Print received servo values
+    // Print received values to Serial Monitor
     Serial.print("Received Data: ");
     Serial.print("Base: ");
     Serial.print(Received_Servo_Value[0]);
@@ -84,6 +83,7 @@ void loop() {
   delay(1);
   if (Counter_to_Refresh >= 10) {    // delay of 10 msec = 1 msec * 10, this will allow smooth movement of servos
     Counter_to_Refresh = 0;
+    // Update base servo position
     if (Received_Servo_Value[0] != Final_Servo_Val[0]) {
       if (Received_Servo_Value[0] > Final_Servo_Val[0]) {
         Final_Servo_Val[0]++;
@@ -94,7 +94,7 @@ void loop() {
       }
       myservo_1.write(180 - Final_Servo_Val[0]);      // adjusted as per app
     }
-
+   // Update shoulder servo position
     if (Received_Servo_Value[1] != Final_Servo_Val[1]) {
       if (Received_Servo_Value[1] > Final_Servo_Val[1]) {
         Final_Servo_Val[1]++;
@@ -105,7 +105,7 @@ void loop() {
       }
       myservo_2.write(Final_Servo_Val[1]);
     }
-
+    // Update elbow servo position
     if (Received_Servo_Value[2] != Final_Servo_Val[2]) {
       if (Received_Servo_Value[2] > Final_Servo_Val[2]) {
         Final_Servo_Val[2]++;
@@ -116,7 +116,7 @@ void loop() {
       }
       myservo_3.write(Final_Servo_Val[2]);
     }
-
+    // Update gripper servo position
     if (Received_Servo_Value[3] != Final_Servo_Val[3]) {
       if (Received_Servo_Value[3] > Final_Servo_Val[3]) {
         Final_Servo_Val[3]++;
