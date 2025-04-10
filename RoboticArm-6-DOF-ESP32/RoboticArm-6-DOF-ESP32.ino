f23a71b4e4f46e7ed84bac1a57d9da0b55164707
SHIVENDER SINGH THAKUR <150267410+Shivenderthakur@users.noreply.github.com>
2025-04-10 05:44:07 -0700

    Update RoboticArm-6-DOF-ESP32.ino

--- RoboticArm-6-DOF-ESP32/RoboticArm-6-DOF-ESP32.ino
+++ RoboticArm-6-DOF-ESP32/RoboticArm-6-DOF-ESP32.ino
@@ -9,11 +9,11 @@ Servo servo04;
 Servo servo05;
 Servo servo06;
 
-BluetoothSerial Bluetooth; // Arduino(RX, TX) - HC-05 Bluetooth (TX, RX)
+BluetoothSerial Bluetooth; 
 
-int servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; // current position
-int servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; // previous position
-int servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50]; // for storing positions/steps
+int servo1Pos, servo2Pos, servo3Pos, servo4Pos, servo5Pos, servo6Pos; 
+int servo1PPos, servo2PPos, servo3PPos, servo4PPos, servo5PPos, servo6PPos; 
+int servo01SP[50], servo02SP[50], servo03SP[50], servo04SP[50], servo05SP[50], servo06SP[50];
 int speedDelay = 20;
 int indx = 0;
 String dataIn = "";
